f2d07e65a324fd74ad949d03aba639848ab610a4
Your Name <you@example.com>
2023-04-26 09:48:29 -0300

    Empresas y Perfil a la mitad

--- screens/EmpresaScreen.js
+++ screens/EmpresaScreen.js
@@ -1,37 +1,116 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import {StyleSheet, TouchableOpacity, Image, View, Text, Linking} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { ScrollView } from "react-native-gesture-handler";
 
-export default function InstructivoScreen({ navigation }) {
-  return (
-      <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn't exist.</Text>
-        <TouchableOpacity
-          onPress={() => navigation.replace("InstructivoScreen")}
-          style={styles.link}
-        >
-          <Text style={styles.linkText}>Go to home screen!</Text>
-        </TouchableOpacity>
-      </View>
+ 
+export default function EmpresaScreen({ navigation }) {
+  const title =
+    "Las empresas cordobesas que forman parte de la Economía del Conocimiento, tienen acceso a los beneficios impositivos y estímulos a la creación de puestos de trabajo que prevé el Régimen Provincial de la Economía del Conocimiento de Córdoba. El conglomerado de Pymes y emprendimientos locales provienen de diversas actividades que producen servicios basados en el conocimiento o conocimiento embebidos en productos.";
+  const subtitle = "Ver 81 empresas adheridas";
+
+  const handleOpenLink = () => {
+    // Abre un enlace en el navegador cuando se hace clic en el botón
+    Linking.openURL(
+      "https://docs.google.com/spreadsheets/d/1lyf6CqgmGUD4aGs9BS3bC91fNRML-n3u9u5HZhlymVk/edit#gid=0"
     );
-}
+  };
+
+  return (
+    <ScrollView>
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/landing.png")}
+        style={styles.logo}
+      />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Empresas cordobesas</Text>
+        <Text style={styles.title}>{title}</Text>
+        <TouchableOpacity style={styles.buttonContainer} onPress={handleOpenLink} accessibilityRole="button">
+          <Ionicons name="arrow-forward-outline" size={24} color="blue" style={{ marginRight: 10 }}/>
+          <Text style={styles.buttonText}>
+            <Text style={[styles.subTitle, styles.blueText]}>{subtitle}</Text>
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.container}>
+      <Image
+        source={require("../assets/images/map.png")}
+        style={styles.map}
+      />
+      </View>  
+      </View>
+    </View>
+    </ScrollView>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
+    justifyContent: "flex-start",
+    paddingTop: 30,
+  },
+  logo: {
+    width: "100%",
+    height: 150,
+    resizeMode: "contain",
+    marginBottom: 10,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+    marginBottom: 10,
+    textAlign: "center",
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+  textContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
   },
-  linkText: {
-    fontSize: 14,
-    color: "#2e78b7",
+  buttonContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 32,
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+    fontSize: 18,
+    color: "#fff",
+    textAlign: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+
+  },
+  subTitle: {
+    textDecorationLine: "underline",
+    fontWeight: "bold",
+  },
+  blueText: {
+    color: "blue",
+  },
+  logo: {
+    width: "100%",
+    height: 150,
+    resizeMode: "contain",
+    marginBottom: 5,
   },
 });
--- screens/PerfilScreen.js
+++ screens/PerfilScreen.js
@@ -1,81 +1,137 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Image, Button, FlatList } from 'react-native';
-import { FontAwesome } from 'react-native-vector-icons';
+import { View, Text, FlatList, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function Container() {
-  
-  const renderItem = ({ item }) => (
-    <TouchableOpacity style={styles.item}>
-      <Text style={styles.itemTitle}>{item.title}</Text>
-    </TouchableOpacity>
+const NOTIFICATIONS = [
+  {
+    id: '1',
+    title: '¡Oferta limitada!',
+    message: 'Solo hoy, descuento del 30% en toda la tienda',
+    date: '2023-04-25',
+  },
+  {
+    id: '2',
+    title: 'Nuevo inventario',
+    message: '¡Tenemos productos frescos y deliciosos en stock!',
+    date: '2023-04-24',
+  },
+  {
+    id: '3',
+    title: 'Horario especial por el feriado',
+    message: 'Mañana abriremos de 10:00am a 4:00pm',
+    date: '2023-04-30',
+  },
+];
+
+const NotificationItem = ({ id, title, message, date, onPressClose }) => {
+  return (
+    <View style={styles.notificationContainer}>
+      <TouchableOpacity onPress={onPressClose} style={styles.closeIconContainer}>
+        <Ionicons name="close-outline" size={24} color="#666" style={styles.closeIcon} />
+      </TouchableOpacity>
+      <Text style={styles.notificationTitle}>{title}</Text>
+      <Text style={styles.notificationMessage}>{message}</Text>
+      <Text style={styles.notificationDate}>{date}</Text>
+    </View>
   );
+};
+
+const NotificationsScreen = () => {
+  const [notifications, setNotifications] = useState(NOTIFICATIONS);
+  const [showContainer, setShowContainer] = useState(false);
+
+  const handleRemoveNotification = (id) => {
+    setNotifications((currentNotifications) =>
+      currentNotifications.filter((notification) => notification.id !== id)
+    );
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.profileContainer}>
-        <Image
-          source={require("../assets/images/macena.png")}
-          style={styles.productoRedondo}
-        />
-        <View style={styles.profileInfoContainer}>
-          <Text style={styles.profileName}>MACENA</Text>
+    <View style={{ flex: 1 }}>
+      {showContainer && (
+        <View style={styles.container}>
+         <Image source={require("../assets/images/macena.png")} style={styles.profilePicture}/>
+          <Text style={styles.companyName}>Macena</Text>
         </View>
-      </View>
-
-      <Button
-        title={"Ver góndola"}
-        color={"transparent"}
-        onPress={() => Alert.alert("Completado", "Compra finalizada con exito")}
-        style={styles.button}
+      )}
+      <TouchableOpacity onPress={() => setShowContainer(!showContainer)}>
+        <Text style={styles.showContainer}>{showContainer ? 'Ocultar contenedor' : 'Mostrar contenedor'}</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={notifications}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <NotificationItem
+            id={item.id}
+            title={item.title}
+            message={item.message}
+            date={item.date}
+            onPressClose={() => handleRemoveNotification(item.id)}
+          />
+        )}
       />
     </View>
   );
 };
-  
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 0,
-    backgroundColor: 'white',
-    margin: 10,
-    padding: 20,
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
+  notificationContainer: {
+    flexDirection: 'column',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 0,
+    marginBottom: 20,
+    position: 'relative',
+    borderStyle: 'solid',
+    backgroundColor: '#fff', // agregando un fondo blanco
   },
-  profileContainer: {
+  container: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 20,
+    backgroundColor: '#f0f0f0',
+    padding: 16,
   },
-  profileName: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginLeft: 10,
-  },
-  profileInfoContainer: {
-    flex: 0,
-    marginLeft: 20,
-  },
-  button: {
-    marginTop: 20,
-    borderWidth: 0,
-    width: 1,
-    height: 1,
-  },
-  productoRedondo: {
-    backgroundColor: 'rgb(232,228,234)',
+  profilePicture: {
+    backgroundColor: '#020202',
     resizeMode: 'center',
-    width: 100,
-    height: 100,
+    width: 130,
+    height: 130,
     borderRadius: 500,
     borderColor: '#020202',
     borderWidth: 3,
+    marginTop: 50,
+    marginLeft: 50,
   },
-  title: {
-    fontSize: 20,
+companyName: {
+  fontWeight: 'bold',
+  fontSize: 24,
+  color: '#0A387D',
+  textAlign: 'center', // centrar horizontalmente
+  alignItems: 'center', // centrar verticalmente
+},
+  showContainer: {
+    alignSelf: 'flex-end',
+    padding: 16,
+    textDecorationLine: 'underline',
+    color: '#0A387D',
+  },
+  notificationTitle: {
+    fontSize: 18,
     fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 0,
+    fontFamily: 'System', // nombre de la fuente personalizada
+  },
+  notificationMessage: {
+    fontSize: 16,
+    fontFamily: 'System', // otro nombre de fuente personalizada
+  },
+  notificationDate: {
+    fontSize: 14,
+    fontFamily: 'System', // nombre de la fuente predeterminada
   },
 });
+
+
+export default NotificationsScreen;
